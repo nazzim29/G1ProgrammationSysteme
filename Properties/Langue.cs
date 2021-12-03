a595e9deeefae914e55ecfc4b501b6586435b459
yasmineexia <yasmine.benoudina.dz@viacesi.fr>
2021-12-03 13:55:14 +0000

    LangueNoImplemented

---
+++ Properties/Langue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave_GUI.Properties
+{
+    public abstract class Langue
+    {
+        public virtual void ToString()
+        {
+            
+        }
+    }
+}
