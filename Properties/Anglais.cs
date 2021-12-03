0b6764b943cc911ed3c3664b5f29dcbf70df2182
yasmineexia <yasmine.benoudina.dz@viacesi.fr>
2021-12-03 16:22:19 +0000

    EN

--- Properties/Anglais.cs
+++ Properties/Anglais.cs
@@ -8,6 +8,17 @@ namespace EasySave_GUI.Properties
 {
     class Anglais : Langue
     {
+        public static string backup_name { get { return "Name"; } }
+        public static string source { get { return "Source"; } }
+        public static string destination { get { return "Destination"; } }
+        public static string type { get { return "Type"; } }
+        public static string crypt { get { return "Encryption"; } }
+        public static string ajouter { get { return "Add"; } }
+        public static string lancer { get { return "Launch"; } }
+        public static string supp { get { return "Delete"; } }
+        public static string quitter { get { return "Exit"; } }
+        public static string sequentiel { get { return "One By one  "; } }
+        public static string simultane { get { return "Simultaneous"; } }
         public override string ToString()
         {
             return "hadja";
